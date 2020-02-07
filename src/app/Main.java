91c42eed3a676d6c8015ce34ae458c9f79e11c0f
nacatcrazy <60526456+nacatcrazy@users.noreply.github.com>
2020-02-07 14:54:00 -0800

    Update Main.java
    Changing the functions to include the menu class.

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,8 +1,27 @@
+/**
+ * @author Nathaniel Adams, Levi Kruse, Jonathan Sandberg, Craig Jones
+ * @email nacatcrazy@gmail
+ * @create date 2020-02-07 14:24:51
+ * @modify date 2020-02-07 14:26:07
+ * @desc This project should not only work between mutliple class to allow the user
+ *       to get the area of multiple functions and if the user would like they can make there own equation.
+ */
+
 package app;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
-        System.out.println("This projects sucks");
+    public static void main(String[] args) {
+        
+        Menu menu = new Menu();
+
+        do {
+
+            menu.Display();
+
+
+        }while(true);
+        
+
+
     }
 }
