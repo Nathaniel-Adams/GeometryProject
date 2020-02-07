2aa707184c32e28366de8b6c9f3ef7e1866b74bd
Berbandis <levi_kruse@hotmail.com>
2020-02-07 14:02:07 -0800

    Update Main.java

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,5 +3,6 @@ package app;
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello Java");
+        System.out.println("This projects sucks");
     }
 }
