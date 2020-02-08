7afe0f71d00d5a0589a6ad5f00ef47ce627a7ad5
nacatcrazy <60526456+nacatcrazy@users.noreply.github.com>
2020-02-07 18:10:42 -0800

    Updating Main.java
    This should include a query User and to be able to quit out of the application

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,7 +2,7 @@
  * @author Nathaniel Adams, Levi Kruse, Jonathan Sandberg, Craig Jones
  * @email nacatcrazy@gmail
  * @create date 2020-02-07 14:24:51
- * @modify date 2020-02-07 14:26:07
+ * @modify date 2020-02-07 18:09:36
  * @desc This project should not only work between mutliple class to allow the user
  *       to get the area of multiple functions and if the user would like they can make there own equation.
  */
@@ -10,16 +10,18 @@
 package app;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         
         Menu menu = new Menu();
 
+        menu.Introduction();
+
         do {
-
             menu.Display();
-
-
-        }while(true);
+            menu.QueryUser();
+            menu.ClearScreen();
+            
+        }while(menu.Continue());
         
 
 
