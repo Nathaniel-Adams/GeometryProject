f452624e6cafd310b459cf39499c403f44fc8590
nacatcrazy <60526456+nacatcrazy@users.noreply.github.com>
2020-02-08 14:07:49 -0800

    Update Main.java
    Updating the main.java to be able to close out the object to save for memory leaks. Make it nice and clean :)!

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,7 +2,7 @@
  * @author Nathaniel Adams, Levi Kruse, Jonathan Sandberg, Craig Jones
  * @email nacatcrazy@gmail
  * @create date 2020-02-07 14:24:51
- * @modify date 2020-02-07 18:09:36
+ * @modify date 2020-02-08 14:06:12
  * @desc This project should not only work between mutliple class to allow the user
  *       to get the area of multiple functions and if the user would like they can make there own equation.
  */
@@ -19,10 +19,13 @@ public class Main {
         do {
             menu.Display();
             menu.QueryUser();
-            menu.ClearScreen();
+            menu.ProcessCommand();
+            Menu.ClearScreen(); // opens clearscreen in a static function.
             
         }while(menu.Continue());
         
+        menu.close();
+        menu = null;
 
 
     }
