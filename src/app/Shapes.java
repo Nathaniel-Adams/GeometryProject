b352a4d30fbbaefd6626c1eae6e7d8f35d4f9d65
nacatcrazy <60526456+nacatcrazy@users.noreply.github.com>
2020-02-08 14:11:19 -0800

    Create Shapes.java
    This is a very empty shell of the file, will add more later.

---
+++ src/app/Shapes.java
@@ -0,0 +1,22 @@
+/**
+ * @author Nathaniel Adams, Levi Kruse, Jonathan Sandberg, Craig Jones
+ * @email nacatcrazy@gmail
+ * @create date 2020-02-08 14:06:36
+ * @modify date 2020-02-08 14:06:36
+ * @desc [description]
+ */
+
+package app;
+
+public class Shapes {
+
+    public Shapes() {
+        
+    }
+
+    public void close() {
+
+    }
+
+
+}
