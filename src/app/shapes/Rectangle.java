012c0e05343c7ca1be0c54b58c6fda099618badb
nacatcrazy <60526456+nacatcrazy@users.noreply.github.com>
2020-02-09 14:07:39 -0800

    Create Rectangle.java
    Very barebone file will add later

---
+++ src/app/shapes/Rectangle.java
@@ -0,0 +1,23 @@
+package app.shapes;
+
+import app.CommonFunctions;
+import java.util.Scanner;
+
+public class Rectangle extends CommonFunctions {
+
+    private Scanner keyboard;
+    private int length;
+    private int width;
+
+//  ====================================
+    public Rectangle(Scanner keyboard) {
+
+        this.keyboard = keyboard;
+        length = width = 0;
+
+    }// Constructor
+//  ===============
+
+
+
+}
