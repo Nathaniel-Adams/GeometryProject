f6cd38001da38b59f04ac998f1969268d422d1c1
nacatcrazy <60526456+nacatcrazy@users.noreply.github.com>
2020-03-13 09:45:36 -0700

    Update Shapes.java

--- src/app/Shapes.java
+++ src/app/Shapes.java
@@ -3,7 +3,9 @@
  * @email nacatcrazy@gmail
  * @create date 2020-02-08 14:06:36
  * @modify date 2020-02-08 14:06:36
- * @desc [description]
+ * @desc This class file will be the one that controlls the other shapes and will close them all 
+ *       all shapes when the class file is closed one step above it.
+ * 
  */
 
 package app;
@@ -15,6 +17,8 @@ public class Shapes extends CommonFunctions {
 //  ===========================================
     private Triangle triangle;
     private Rectangle rectangle;
+    private Prism prism;
+    private Cube cube;
 //  ===========================================
 
 //  =================================
@@ -22,20 +26,37 @@ public class Shapes extends CommonFunctions {
 
         triangle = new Triangle(keyboard);
         rectangle = new Rectangle(keyboard);
+        prism = new Prism(keyboard);
+        cube = new Cube(keyboard);
+
     }// Constructor
 //  ===============
+
+    public void Cube() throws Exception {
+
+        Cube.NowaitClearScreen();
+
+        do {
+            cube.MenuDisplay2d("Cube");
+            cube.QueryUser();
+            Cube.ClearScreen();
+        } while(false);
+    }
 
 //  ==========================================
     public void Rectangle() throws Exception {
 
-        Rectangle.ClearScreen();
+        Rectangle.NowaitClearScreen();
 
         do {
             rectangle.MenuDisplay2d("Rectangle");
+            rectangle.QueryUser();
+            rectangle.ProcessCommand();
 
-            Triangle.ClearScreen();
+            if(rectangle.Goback()) 
+                Rectangle.ClearScreen();
 
-        } while(false);
+        } while(rectangle.Goback());
 
     }// Rectangle Function
 //  ======================
@@ -43,7 +64,7 @@ public class Shapes extends CommonFunctions {
 //  =========================================
     public void Triangle() throws Exception {
 
-        Triangle.ClearScreen();
+        Triangle.NowaitClearScreen();
 
         do {
             triangle.Menu();
@@ -51,12 +72,30 @@ public class Shapes extends CommonFunctions {
             triangle.ProcessCommand();
 
             if (triangle.Goback())
-                Triangle.ClearScreen();
+                Triangle.NowaitClearScreen();
 
         } while(triangle.Goback());
 
     }// Triangle Function
 //  =====================
+
+//  ======================================
+    public void Prism() throws Exception {
+        Prism.NowaitClearScreen();
+
+        do {
+
+            prism.Menu();
+            prism.QueryUser();
+            prism.ProcessCommand();
+
+            if (prism.Goback()) 
+                Prism.ClearScreen();
+
+        } while (prism.Goback());
+
+    }// Function Prism
+//  ==================
 
 //  =====================
     public void close() {
